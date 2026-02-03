c966134133f288328b36040fd41413bf0224dc43
karianyu <mibangoe041@gmail.com>
2026-02-03 12:44:43 +0300

    after migrate

--- crm/hooks.py
+++ crm/hooks.py
@@ -254,7 +254,7 @@ ignore_links_on_delete = ["Failed Lead Sync Log"]
 # ]
 
 after_migrate = [
-	"crm.fcrm.doctype.fcrm_settings.fcrm_settings.after_migrate",
+	# "crm.fcrm.doctype.fcrm_settings.fcrm_settings.after_migrate",
 	"crm.api.whatsapp.add_roles",
 ]
 
